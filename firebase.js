fdd8d71c7848226e87be946a73750927c8a3df0d
Katherine Gacusan <gacusankatherine0514@gmail.com>
2024-03-05 11:40:14 -0800

    added firebase and metro

---
+++ firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAIdiPCPVc_UrwjaZTin2BgWQM-iXeCqnQ",
+  authDomain: "itas274finalproject-642b8.firebaseapp.com",
+  projectId: "itas274finalproject-642b8",
+  storageBucket: "itas274finalproject-642b8.appspot.com",
+  messagingSenderId: "103790494714",
+  appId: "1:103790494714:web:bf67bee7a9a61ec4819a43",
+  measurementId: "G-486XHBNJ8L",
+};
+
+const app = initializeApp(firebaseConfig);
