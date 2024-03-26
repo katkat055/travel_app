86a3f8f85f85f9ec60165822e42ab2710ae99a59
Katherine Gacusan <gacusankatherine0514@gmail.com>
2024-03-26 11:15:36 -0700

    calendar not working

--- components/Home.js
+++ components/Home.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, Button, Pressable, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, Alert } from "react-native";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../config/firebase.js";
+import * as Calendar from "expo-calendar";
 import { styles } from "../styles.js";
 
 export default function Home({ navigation, route }) {
@@ -31,6 +32,41 @@ export default function Home({ navigation, route }) {
     const date = new Date(timestamp.seconds * 1000);
     return date.toLocaleString();
   };
+
+  // const addEventToCalendar = async (destination, tripDate, endDate, budget) => {
+  //   try {
+  //     const { status } = await Calendar.requestCalendarPermissionsAsync();
+  //     if (status === "granted") {
+  //       console.log("Permissions granted. Fetching available calendars...");
+  //       const calendars = await Calendar.getCalendarsAsync(
+  //         Calendar.EntityTypes.EVENT
+  //       );
+  //       const defaultCalendar =
+  //         calendars.find((calendar) => calendar.isPrimary) || calendars[0];
+  //       if (defaultCalendar) {
+  //         const eventConfig = {
+  //           destination,
+  //           tripDate,
+  //           endDate,
+  //           budget,
+  //         };
+  //         console.log("eventConfig:", eventConfig);
+  //         const eventId = await Calendar.createEventAsync(
+  //           defaultCalendar.id,
+  //           eventConfig
+  //         );
+  //         console.log(eventId);
+  //         Alert.alert("Success", "Event added to your calendar");
+  //       } else {
+  //         console.warn("No available calendars found.");
+  //       }
+  //     } else {
+  //       console.warn("Calendar permission not granted.");
+  //     }
+  //   } catch (error) {
+  //     console.warn(error);
+  //   }
+  // };
 
   if (loading) {
     return (
@@ -68,6 +104,19 @@ export default function Home({ navigation, route }) {
             >
               <Text style={{ color: "white" }}>View Trip</Text>
             </TouchableOpacity>
+            {/* <TouchableOpacity
+              onPress={() =>
+                addEventToCalendar(
+                  trip.trip.destination,
+                  trip.trip.tripDate,
+                  trip.trip.endDate,
+                  trip.trip.budget
+                )
+              }
+              style={styles.btn}
+            >
+              <Text style={{ color: "white" }}>Add to Calendar</Text>
+            </TouchableOpacity> */}
           </View>
         ))}
       </View>
